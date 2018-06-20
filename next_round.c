aaf0d024eceba81ffc215f9acdae2e3aa0163ed2
krishremya <krishremya99@gmail.com>
2018-06-20 23:39:04 +0530

    Codeforces

--- next_round.c
+++ next_round.c
@@ -1,3 +1,5 @@
+/*The aim of the problem is to find the participants who are eligible to attend the next round. For attaining, the number of participants(n) and the k th player is inputted and after that an array is entered with n members and the aim is find whether a[i] th value is greater than or equal to the kth payers value. If its encountered then counter is added.*/
+
 #include<stdio.h>
 int main()
 {
