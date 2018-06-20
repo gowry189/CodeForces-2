559dcb45d486c621b6360cf2b4686f274807ea44
krishremya <krishremya99@gmail.com>
2018-06-20 23:30:30 +0530

    Codeforces

--- team.c
+++ team.c
@@ -1,3 +1,6 @@
+/*There are three friends and they have to input three numbers either 1 or 0. If the inputted value is one then she is sure about her solution for a problem. If sum of the three found to be greater or equal to 2 then they can solve the problem. So the aim of the programmer is to find the number of times they can find the solution if they have to solve n number of problem.*/ 
+
+
 #include<stdio.h>
 int main()
 {
