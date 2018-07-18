d85e65bf56948757a8738c3ba20b47c49586fda5
krishremya <krishremya99@gmail.com>
2018-07-18 22:03:19 +0530

    CodeForces

---
+++ the_sum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    int z, x = 0, y = 1, sum=1;
+    int n ;
+    cin>>n;
+    for( int i = 2; i <= n; i++)
+    {
+        z= x+y;
+        sum += z;
+        x= y;
+        y= z;
+    }
+    cout<<sum;
+    return 0;
+}
