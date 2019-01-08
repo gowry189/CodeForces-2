c4d142ef22290574e6be966b5c0ed56aac2ec70a
krishremya <krishremya99@gmail.com>
2019-01-08 11:10:08 +0530

    SnowBall

---
+++ snowball.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int w, h;
+    int u1, u2;
+    int d1, d2;
+    cin>> w >> h;
+    cin >> u1 >> d1;
+    cin >> u2 >> d2;
+    for(int i = h; i >= 0 ; i--) {
+        w = w + i;
+        if(d1 != d2) {
+            if(i == d1 && d1 <= h ) {
+                w = w - u1;
+            } else if (i == d2 && d2 <=h) {
+                w = w - u2;
+            }
+        }
+        w = max ( 0 , w);
+    } 
+    if(h <= 0)
+        cout << 0 ;
+    else 
+        cout << w;
+    return 0;
+}
